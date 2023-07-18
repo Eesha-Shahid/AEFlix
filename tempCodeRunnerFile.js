16ddab35fce67474b30f3205d1e48112d412309b
Eesha Shahid <eesha.shahid14@gmail.com>
2023-07-19 03:48:53 +0500

    Uploaded Project

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+var dateString = new Date();
+// console.log(dateString.toDateString());
+
+// var next = new Date();
+// next.setDate(new Date().getDate() + 7);
+// console.log(next.toDateString().split(' ').slice(1).join(' '));
